f8fbc8653150a2e57fdaa3f3fe098752cd64fe3e
yopox <yopoxdev@gmail.com>
2023-04-27 21:41:38 +0200

    setup: Fix MRMOTEXT path

--- src/loading.rs
+++ src/loading.rs
@@ -23,7 +23,7 @@ pub struct Textures {
     pub icons: Handle<TextureAtlas>,
 
     #[asset(texture_atlas(tile_size_x = 8., tile_size_y = 8., columns = 32, rows = 32, padding_x = 0., padding_y = 0.))]
-    #[asset(path = "../../imperatrices/assets/MRMOTEXT EX.png")]
+    #[asset(path = "MRMOTEXT EX.png")]
     pub mrmotext: Handle<TextureAtlas>,
 
     #[asset(path = "slot.png")]
